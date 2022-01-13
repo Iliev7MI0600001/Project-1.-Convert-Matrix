3ebab2c5921a08f20ba1f4cf00b18b15525144f2
Iliev7MI0600001 <dimitar.iliev.21g@gmail.com>
2022-01-13 10:10:26 +0200

    Adding menu and text files

---
+++ menu.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+	int choice;
+	do
+	{
+		cout<<"Please choose one of the following:"<<endl;
+		cout<<"0. Quit"<<endl;	
+		cout<<"1. Review The Entry Data;"<<endl;
+		cout<<"2. Change One Number"<<endl;
+		cout<<"3. Review The Entry Functions;"<<endl;
+		cout<<"4. Change One Entry Function;"<<endl;
+		cout<<"5. Commit A Matrix Calculation;"<<endl;
+		cout<<"6. Choose A Carry Mode"<<endl;
+		cout<<"7. Save"<<endl;
+		cin>>choice;
+		
+		switch(choice)
+		{
+			case 0:
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+		}	
+	}
+	while (choice!=0);
+	return 0;
+}
